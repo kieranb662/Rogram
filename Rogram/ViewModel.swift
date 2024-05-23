10a7b9c19615dade0de20354ea37f3c685b03117
Kieran Brown <kb6627500@gmail.com>
2024-05-23 16:32:18 -0400

    Added a viewModel with fetch functionality

---
+++ Rogram/ViewModel.swift
@@ -0,0 +1,26 @@
+// Swift toolchain version 5.0
+// Running macOS version 14.5
+// Created on 5/23/24.
+//
+// Author: Kieran Brown
+//
+
+import Foundation
+
+
+@Observable
+class ViewModel {
+    let endpoint = URL(string: "https://jsonplaceholder.typicode.com/album/1/photos")
+    var models: [Model] = []
+    
+    
+    func fetch() async throws -> [Model] {
+        guard let endpoint else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: endpoint)
+        let models = try JSONDecoder().decode([Model].self, from: data)
+        return models
+    }
+}
