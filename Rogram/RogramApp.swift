84042544241f3114388b65eef0b92e92b5ddf78b
Kieran Brown <kb6627500@gmail.com>
2024-05-23 16:02:08 -0400

    Initial Commit

---
+++ Rogram/RogramApp.swift
@@ -0,0 +1,17 @@
+// Swift toolchain version 5.0
+// Running macOS version 14.5
+// Created on 5/23/24.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+@main
+struct RogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
